263895c77f3a04b584c8b11fee46eff2add9dcf5
Rodney van den Velden <rodneymichael2002@gmail.com>
2023-03-24 11:54:45 +0000

    2022: 20/50

---
+++ 2022/day10/mod.ts
@@ -0,0 +1,53 @@
+const input = Deno.readTextFileSync('./input.txt').trim().split('\n');
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+const cpu = {
+  x: 1,
+  pc: 0,
+  signal: 0,
+  curRow: 0,
+  screen: new Array(6).fill(null).map((_) => new Array(40).fill('⬛')),
+}
+
+async function execute(opcode: string) {
+  await delay(50);
+
+  if (opcode === 'noop') {
+    cycle();
+  } else {
+    cycle();
+    cycle();
+
+    const amount = parseInt(opcode.split(' ')[1]);
+    cpu.x += amount;
+  }
+}
+
+function cycle() {
+  cpu.pc++;
+
+  if ([20, 60, 100, 140, 180, 220].includes(cpu.pc)) {
+    cpu.signal += cpu.pc * cpu.x;
+  }
+
+  drawScreen([cpu.x - 1, cpu.x, cpu.x + 1]);
+}
+
+function drawScreen(spritePos: number[]) {
+  const pos = (cpu.pc - 1) % 40;
+
+  if (spritePos.includes(pos)) {
+    cpu.screen[cpu.curRow][pos] = '⬜';
+  }
+
+  Deno.stdout.writeSync(new TextEncoder().encode(pos === 0 ? `\n${cpu.screen[cpu.curRow][pos]}` : `${cpu.screen[cpu.curRow][pos]}`));
+  if (pos === 39) {
+    cpu.curRow === 5 ? cpu.curRow = 0 : cpu.curRow++;
+  }
+}
+
+for (const opcode of input) {
+  await execute(opcode);
+}
+
+console.log(`\nPart one: ${cpu.signal}`);
